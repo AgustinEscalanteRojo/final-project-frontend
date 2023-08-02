faeb096fac07f5c99855c3fd5c45564077845c30
MarioFalcon <maok.seven@gmail.com>
2023-08-02 20:19:45 +0200

    feat: Add view dashboard

---
+++ src/views/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { FC, memo } from 'react'
+import useLogic from './logic'
+import { Container } from './styles'
+
+const Dashboard: FC = () => {
+  return <Container></Container>
+}
+
+export default memo(Dashboard)
