1ef9350f67207c9d7dcccc3755665a300dba6ced
MarioFalcon <maok.seven@gmail.com>
2023-08-05 06:36:31 +0200

    feat: Add view profile

---
+++ src/views/Profile/profile.tsx
@@ -0,0 +1,24 @@
+import { FC, memo, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { PerfilContainer, Content } from './styles'
+
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import VideoBackground from '../../components/VideoBackground/videoBackground'
+import type { Props } from './types'
+
+const Profile: FC<Props> = ({ onLogout }) => {
+  return (
+    <PerfilContainer>
+      <Header onLogout={onLogout} />
+      <Content>
+
+        
+      </Content>
+      <Footer />
+      <VideoBackground videoSrc="/back.mp4" />
+    </PerfilContainer>
+  )
+}
+
+export default memo(Profile)
