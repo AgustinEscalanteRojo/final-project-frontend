40cd6dca126349e52c76ebaa4bb70f5623214946
MarioFalcon <maok.seven@gmail.com>
2023-08-15 02:00:40 +0200

    feat: Add component ImageBackground

---
+++ src/components/ImageBackground/index.tsx
@@ -0,0 +1,13 @@
+import { FC, memo } from 'react';
+import { BackgroundContainer, Image } from './styles'; 
+import type { Props } from './types';
+
+const ImageBackground: FC<Props> = ({ imageSrc }) => {
+  return (
+    <BackgroundContainer>
+      <Image src={imageSrc} alt="Background Image" />
+    </BackgroundContainer>
+  );
+}
+
+export default memo(ImageBackground);
---
+++ src/components/ImageBackground/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const BackgroundContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
