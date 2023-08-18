a03ccc83546ff85ffdbc7bf136d00111639d3228
VaroLim <trabajosydemascosas@gmail.com>
2023-08-18 21:56:45 +0200

    feat: Implement search icon and recipe-search-area for the post and make it responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,87 @@
+// import React, { FC, memo, useCallback, useState, useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import type { Props } from './types'
+// import { logout } from '../../services/api/auth'
+// import Image from '../Image'
+// import IconButton from '@mui/material/IconButton'
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+// import LogoutIcon from '@mui/icons-material/Logout'
+// import MenuIcon from '@mui/icons-material/Menu'
+// import Button from '../Button'
+// import { ButtonController, Container, MobileButtonController } from './styles'
+// import { MobileIconButton } from './styles'
+
+// const Header: FC<Props> = ({ onLogout }) => {
+//   const navigate = useNavigate()
+//   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+//   const [isMobileView, setIsMobileView] = useState(window.innerWidth < 768)
+
+//   const handleGoToProfile = useCallback(() => {
+//     navigate('/profile')
+//     setIsMobileMenuOpen(false)
+//   }, [navigate])
+
+//   const handleLogout = useCallback(async () => {
+//     await logout()
+//     onLogout()
+//     navigate('/login')
+//   }, [navigate, onLogout])
+
+//   const toggleMobileMenu = () => {
+//     setIsMobileMenuOpen((prevState) => !prevState)
+//   }
+
+//   useEffect(() => {
+//     const handleWindowResize = () => {
+//       setIsMobileView(window.innerWidth < 768)
+//     }
+
+//     window.addEventListener('resize', handleWindowResize)
+
+//     return () => {
+//       window.removeEventListener('resize', handleWindowResize)
+//     }
+//   }, [])
+
+//   return (
+//     <Container>
+//       <Image src="/logo&tipo.png" alt="logo&tipo" variant="logoHeader" />
+//       <ButtonController>
+//         {isMobileView ? (
+//           <MobileButtonController>
+//             <IconButton size="large" onClick={toggleMobileMenu}>
+//               <MenuIcon sx={{ color: 'white', fontSize: 40 }} />
+//             </IconButton>
+//             {isMobileMenuOpen && (
+//               <div>
+//                 <MobileIconButton size="large" onClick={handleGoToProfile}>
+//                   <AccountCircleIcon sx={{ color: 'white', fontSize: 40 }} />
+//                 </MobileIconButton>
+//                 <MobileIconButton size="large" onClick={handleLogout}>
+//                   <LogoutIcon sx={{ color: 'white', fontSize: 40 }} />
+//                 </MobileIconButton>
+//               </div>
+//             )}
+//           </MobileButtonController>
+//         ) : (
+//           <>
+//             <IconButton size="large" onClick={handleGoToProfile}>
+//               <AccountCircleIcon sx={{ color: 'white', fontSize: 40 }} />
+//             </IconButton>
+//             <MobileIconButton size="large" onClick={handleLogout}>
+//               <LogoutIcon sx={{ color: 'white', fontSize: 40 }} />
+//             </MobileIconButton>
+//           </>
+//         )}
+//       </ButtonController>
+//     </Container>
+//   )
+// }
+
+// export default memo(Header)
+
 import React, { FC, memo, useCallback, useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import type { Props } from './types'
 import { logout } from '../../services/api/auth'
 import Image from '../Image'
@@ -7,13 +89,14 @@ import IconButton from '@mui/material/IconButton'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import LogoutIcon from '@mui/icons-material/Logout'
 import MenuIcon from '@mui/icons-material/Menu'
-import Button from '../Button'
+import SearchIcon from '@mui/icons-material/Search'
 import { ButtonController, Container, MobileButtonController } from './styles'
-import { MobileIconButton } from './styles'
+import { MobileIconButton, SearchArea } from './styles'
 
 const Header: FC<Props> = ({ onLogout }) => {
   const navigate = useNavigate()
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  const [isSearchOpen, setIsSearchOpen] = useState(false) // Nuevo estado para el área de búsqueda
   const [isMobileView, setIsMobileView] = useState(window.innerWidth < 768)
 
   const handleGoToProfile = useCallback(() => {
@@ -31,6 +114,10 @@ const Header: FC<Props> = ({ onLogout }) => {
     setIsMobileMenuOpen((prevState) => !prevState)
   }
 
+  const toggleSearch = () => {
+    setIsSearchOpen((prevState) => !prevState) // Cambia el estado del área de búsqueda
+  }
+
   useEffect(() => {
     const handleWindowResize = () => {
       setIsMobileView(window.innerWidth < 768)
@@ -45,7 +132,9 @@ const Header: FC<Props> = ({ onLogout }) => {
 
   return (
     <Container>
-      <Image src="/logo&tipo.png" alt="logo&tipo" variant="logoHeader" />
+      <Link to="/dashboard">
+        <Image src="/logo&tipo.png" alt="logo&tipo" variant="logoHeader" />
+      </Link>
       <ButtonController>
         {isMobileView ? (
           <MobileButtonController>
@@ -71,9 +160,24 @@ const Header: FC<Props> = ({ onLogout }) => {
             <MobileIconButton size="large" onClick={handleLogout}>
               <LogoutIcon sx={{ color: 'white', fontSize: 40 }} />
             </MobileIconButton>
+            {/* Nuevo botón de búsqueda */}
+            <IconButton size="large" onClick={toggleSearch}>
+              <SearchIcon sx={{ color: 'white', fontSize: 40 }} />
+            </IconButton>
           </>
         )}
       </ButtonController>
+      {/* Área de búsqueda */}
+      {isSearchOpen && (
+        <SearchArea>
+          <input
+            type="text"
+            placeholder="Buscar recetas..."
+            // value={searchQuery}
+            // onChange={handleSearchChange}
+          />
+        </SearchArea>
+      )}
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,46 @@
+// import styled from 'styled-components'
+// import theme from '../../styles/theme'
+// import IconButton from '@mui/material/IconButton'
+
+// export const MobileIconButton = styled(IconButton)`
+//   background-color: black;
+//   border-radius: 50%;
+// `
+
+// export const Container = styled.header`
+//   display: flex;
+//   flex-direction: column;
+//   background-color: ${theme.colors.black};
+//   border-bottom: 2px solid ${theme.colors.buff};
+//   width: 100%;
+//   height: 70px;
+//   position: fixed;
+//   z-index: 1;
+
+//   @media (min-width: 768px) {
+//     flex-direction: row;
+//     justify-content: space-between;
+//     height: 60px;
+//     padding: 20px;
+//   }
+// `
+
+// export const ButtonController = styled.div`
+//   display: flex;
+//   gap: 10px;
+
+//   @media (min-width: 768px) {
+//     gap: 30px;
+//     align-self: center;
+//   }
+// `
+
+// export const MobileButtonController = styled.div`
+//   display: flex;
+//   align-items: center;
+//   gap: 10px;
+// `
+
 import styled from 'styled-components'
 import theme from '../../styles/theme'
 import IconButton from '@mui/material/IconButton'
@@ -39,4 +82,35 @@ export const MobileButtonController = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+`
+
+export const SearchButton = styled(IconButton)`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: flex; /* Cambiar de block a flex */
+    align-items: center; /* Alinear el botón verticalmente */
+    gap: 10px; /* Agregar espacio entre el botón y los iconos */
+  }
+`
+
+export const SearchArea = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background-color: ${theme.colors.black};
+  padding: 10px;
+  transition: all 0.3s ease-in-out;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 100%;
+    left: 50%; /* Centrar el área de búsqueda horizontalmente */
+    transform: translateX(-50%); /* Desplazarla hacia la izquierda */
+    background-color: ${theme.colors.black}; /* Mantener el fondo negro */
+    padding: 10px;
+  }
 `
