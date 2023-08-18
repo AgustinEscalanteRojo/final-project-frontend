10bddc332ee93d9842208bd2b2a8fff387de9e39
Agustin <agustinescalanterojo@gmail.com>
2023-08-18 19:06:14 +0200

    feat: Add view Faq

---
+++ src/views/Faq/index.tsx
@@ -0,0 +1,20 @@
+import { FC, memo} from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import { PerfilContainer, Content, Backgroundcolor } from './styles'
+import type { Props } from './types'
+
+const Faq: FC<Props> = ({ onLogout }) => {
+  return (
+    <PerfilContainer>
+      <Header onLogout={onLogout} />
+      <Content>
+        
+      </Content>
+      <Footer />
+      <Backgroundcolor />
+    </PerfilContainer>
+  )
+}
+
+export default memo(Faq)
