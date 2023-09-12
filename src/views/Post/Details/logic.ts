5982e4e4d046ffa91dad5fc0c0c6f422df7a695e
Reyeslim <reyeslimones@gmail.com>
2023-09-12 18:43:22 +0200

    feat: implemented avatar on details view

--- src/views/Post/Details/logic.ts
+++ src/views/Post/Details/logic.ts
@@ -1,11 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { NormalizedUserPostComment } from '../../../models/UserPostComment'
+import { User } from '../../../models/User'
 import { getComments, sendComment } from '../../../services/api/comments'
+import { getAllUsers } from '../../../services/api/user'
 import { Props } from './types'
+import { useNavigate } from 'react-router-dom'
 
 const useLogic = (post: Props['post']) => {
+  const navigate = useNavigate()
+  const [users, setUsers] = useState<User[]>([])
   const [comment, setComment] = useState('')
   const [comments, setComments] = useState<NormalizedUserPostComment[]>([])
+
+  const fetchUsers = useCallback(async () => {
+    try {
+      const userInfo = await getAllUsers()
+      setUsers(userInfo)
+    } catch (error) {
+      console.error('Error fetching user data:', error)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers])
 
   const handleCommentChange = (e: any) => {
     setComment(e.target.value)
@@ -47,7 +65,38 @@ const useLogic = (post: Props['post']) => {
     }
   }, [post])
 
-  return { comment, comments, handleCommentChange, handleCommentSubmit }
+  function getRandomPastelColor() {
+    const pastelColors = [
+      '#FFD1DC', // Light Pink
+      '#FFABAB', // Light Red
+      '#FFC3A0', // Light Orange
+      '#FF677D', // Light Coral
+      '#D4A5A5', // Light Mauve
+      '#392F5A', // Light Indigo
+      '#31A2AC', // Light Teal
+      '#61C0BF', // Light Cyan
+      '#6B4226', // Light Brown
+      '#D9BF77', // Light Yellow
+    ]
+
+    return pastelColors[Math.floor(Math.random() * pastelColors.length)]
+  }
+
+  const handleGoToProfile = useCallback(() => {
+    navigate(`/users/${post?.userId}`)
+  }, [navigate])
+
+  const creatorUser = users.find((user) => user._id === post?.userId)
+
+  return {
+    comment,
+    comments,
+    handleCommentChange,
+    handleCommentSubmit,
+    getRandomPastelColor,
+    handleGoToProfile,
+    creatorUser,
+  }
 }
 
 export default useLogic
